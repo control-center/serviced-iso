4a9478728223f7802fe218450cbd5656cc4be79c
Cornelius Toole <cornelius.toole@gmail.com>
2016-12-08 16:20:04 -0600

    WIP ...

--- build-zenoss-centos-iso.sh
+++ build-zenoss-centos-iso.sh
@@ -3,6 +3,25 @@
 # Step 1 Build Dependencies tarball
 # inputs: centos-7.2.1511 iso, or centos-7.2.1511 VM
 # outputs: tarball of RPMS,
+curl -sO http://artifacts.zenoss.eng/isos/prereqs/zenoss-centos-7.2.1511.tar.gz
+if [ -e zenoss-centos-7.2.1511.tar.gz ]
+then
+  pushd prereqs
+  tar -xzvf zenoss-centos-7.2.1511.tar.gz
+  popd
+else
+  packer -machine-reable build -only=virtualbox-iso vm-dependencies.json
+  # upload newly created vm file to artifacts
+fi
+
+# The next two packer scripts can probably be merged into one build script
+# that can run in parallel, either as two parallel builders or as concurrent
+# processes within a provisioner job
+packer -machine-readable build -only=virtualbox-ovf vm-get-centos7-base-pkgs.json
+# check if centos7-base-pkgs.tar.gz exists
+packer -machine-readable build -only=virtualbox-ovf vm-get-update-pkgs.json
+# check if centos7-rpm-updates.tar.gz exists
+# merge RPMs tarballs into single tarball with flat directory tree
 
 # Step 2 Build iso-build image
 # inputs: makeiso.sh, centos7-rpms.tar.gz
