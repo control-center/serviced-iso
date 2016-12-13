ea0f667e0197bbb109c76c648c9e34923e729022
Cornelius Toole <cornelius.toole@gmail.com>
2016-12-13 16:39:14 -0600

    fix error msg, change output dir
      - fix error msg
      - create output dir if it doesn't exist
      - d/l checksum file into output folder

--- jenkinsTestServicedBuildDeps.sh
+++ jenkinsTestServicedBuildDeps.sh
@@ -4,13 +4,17 @@ set -x
 
 if [ -z ${SERVICED_RPM} ]
 then
-  echo "ERROR: ${SERVICED_RPM} is undefined"
+  echo "ERROR: SERVICED_RPM is undefined"
   exit 1
+fi
+
+if [ ! -d ./output ]; then
+    mkdir -p output
 fi
 
 SERVICED_ARTIFACT_BASENAME=serviced-1.2.0-1-stable-centos7.2.1511-bld-12
 SERVICED_CENTOS_ISO_URL=http://artifacts.zenoss.eng/isos/serviced/${SERVICED_ARTIFACT_BASENAME}.iso
-CHECKSUM_FILEPATH=${SERVICED_ARTIFACT_BASENAME}.md5sum.txt
+CHECKSUM_FILEPATH=output/${SERVICED_ARTIFACT_BASENAME}.md5sum.txt
 
 wget -q http://artifacts.zenoss.eng/isos/serviced/${SERVICED_ARTIFACT_BASENAME}.md5sum.txt -O ${CHECKSUM_FILEPATH}
 if [ ! -f ${CHECKSUM_FILEPATH} ]
@@ -24,6 +28,6 @@ SERVICED_CENTOS_ISO_CHECKSUM=`cat ${CHECKSUM_FILEPATH} | grep iso | awk '{print 
 packer -machine-readable build -force -only=virtualbox-iso \
   -var iso_url=${SERVICED_CENTOS_ISO_URL} \
   -var iso_checksum=${SERVICED_CENTOS_ISO_CHECKSUM} \
-  -var outputdir=./test-iso-output \
+  -var outputdir=./output \
   -var cc_rpm=${SERVICED_RPM} \
   test-centos-base.json
