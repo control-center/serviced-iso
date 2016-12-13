ab357889ffd81edca4a29cdb0fbc860850ce498e
Cornelius Toole <cornelius.toole@gmail.com>
2016-12-13 16:49:48 -0600

    error out if CC RPM install failed, print more messages

--- test-serviced-deps.sh
+++ test-serviced-deps.sh
@@ -4,3 +4,12 @@ set -x
 
 cd $CC_RPM_DIR
 sudo yum -y localinstall --disablerepo=\* --enablerepo=zenoss-mirror ${CC_RPM_FILENAME}
+
+if [ "$?" -ne 0]
+then
+  echo "Error: Serviced dependency verifcation failed"
+  exit 1
+fi
+
+echo "${CC_RPM_FILENAME} install succeeded."
+echo "JOB DONE"
