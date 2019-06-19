74f84ec4e3070118a4cce83c35c81d6811009b2e
Oleksandr Styhar <ostyhar@zenoss.com>
2019-06-19 10:10:27 +0300

    ZEN-31891: Use Centos 7.6

--- jenkinsTestServicedBuildDeps.sh
+++ jenkinsTestServicedBuildDeps.sh
@@ -17,7 +17,7 @@ export PACKER_CACHE_DIR="${HOME}/packer_cache"
 export PACKER_LOG=1
 export PACKER_LOG_PATH="${BUILD_DIR}/packer-${SERVICED_RPM}.log"
 
-SERVICED_ARTIFACT_BASENAME=serviced-1.6.5-0.0.354.unstable-unstable-centos7.2.1511-bld-49
+SERVICED_ARTIFACT_BASENAME=serviced-1.6.5-0.0.354.unstable-unstable-centos7.6.1810-bld-50
 SERVICED_CENTOS_ISO_URL=http://artifacts.zenoss.eng/isos/serviced/${SERVICED_ARTIFACT_BASENAME}.iso
 CHECKSUM_FILEPATH=output/${SERVICED_ARTIFACT_BASENAME}.md5sum.txt
 
