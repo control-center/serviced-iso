e05d1bae6a552405a44d86c5421bb591b88144f8
azam <deerwarlord@gmail.com>
2024-10-23 22:06:39 +0300

    update curl redirect

--- get-update-pkgs.sh
+++ get-update-pkgs.sh
@@ -55,7 +55,7 @@ yumdownloader --resolve bind-utils
 tar -czvf ../centos7-os-rpms.tar.gz ./*.rpm
 
 # Install the Zenoss repo so
-curl -sO http://get.zenoss.io/yum/zenoss-repo-1-1.x86_64.rpm
+curl -L -sO http://get.zenoss.io/yum/zenoss-repo-1-1.x86_64.rpm
 sudo yum localinstall -y zenoss-repo-1-1.x86_64.rpm
 
 sudo chmod 777 /etc/yum.repos.d
